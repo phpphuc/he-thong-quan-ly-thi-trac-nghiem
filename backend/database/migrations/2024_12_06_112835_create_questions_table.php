3b40fcb6397f41e28108acba94077998a6b6f52e
nnduy223 <20520471@gm.uit.edu.vn>
2024-12-06 19:36:54 +0700

    Update 2024_12_06_112835_create_questions_table.php

--- backend/database/migrations/2024_12_06_112835_create_questions_table.php
+++ backend/database/migrations/2024_12_06_112835_create_questions_table.php
@@ -4,30 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateQuestionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->id('questionid');
+            $table->string('subjectid');
+            $table->unsignedBigInteger('teacherid');
             $table->text('question');
-            $table->string('option1');
-            $table->string('option2');
-            $table->string('option3');
-            $table->string('option4');
-            $table->integer('correct_option');
+            $table->enum('level', ['EASY', 'NORMAL', 'HARD']);
+            $table->string('rightanswer');
+            $table->string('answer_a');
+            $table->string('answer_b');
+            $table->string('answer_c');
+            $table->string('answer_d');
             $table->timestamps();
+            
+            $table->foreign('teacherid')->references('Teacherid')->on('teachers')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('questions');
     }
-};
+}
