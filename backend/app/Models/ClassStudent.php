2ebfd4575decd4a57bdc69aff90632d2862fd9df
phpphuc <19522036@gmail.com>
2024-12-13 16:25:36 +0700

    Sửa theo quy ước mặc định của laravel

--- backend/app/Models/ClassStudent.php
+++ backend/app/Models/ClassStudent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Class_Student extends Model
+class ClassStudent extends Model
 {
     use HasFactory;
     protected $table = 'class_student';
