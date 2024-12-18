0949eb522c3abe0b5c32c1508c6877bfac48807f
nnduy223 <20520471@gm.uit.edu.vn>
2024-12-18 14:55:20 +0700

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\QuestionController;
 use App\Http\Controllers\API\V1\SubjectController;
 use App\Http\Controllers\API\V1\ExamController;
-
+use App\Http\Controllers\API\V1\ResultController;
 Route::prefix('v1')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
@@ -32,4 +32,14 @@ Route::prefix('v1')->group(function () {
     Route::post('/exams', [ExamController::class, 'createExam']);
 
     Route::post('/exams/{id}/submit', [ExamController::class, 'submitExam']);
+
+    Route::get('/results', [ResultController::class, 'index']);
+    Route::get('/results/{id}', [ResultController::class, 'show']);
+    Route::post('/results', [ResultController::class, 'store']);
+    Route::put('/results/{id}', [ResultController::class, 'update']);
+    Route::get('/results/student/{studentId}', [ResultController::class, 'studentHistory']);
+    Route::get('/results/exam/{examId}/report', [ResultController::class, 'examReport']);
+
+   
+
 });
