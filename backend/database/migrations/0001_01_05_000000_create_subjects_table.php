c696238df35a7f21a8c9685caa456de79a19a487
nnduy223 <20520471@gm.uit.edu.vn>
2024-12-30 18:38:58 +0700

    Update 0001_01_05_000000_create_subjects_table.php

--- backend/database/migrations/0001_01_05_000000_create_subjects_table.php
+++ backend/database/migrations/0001_01_05_000000_create_subjects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('subject_name');
+            $table->foreignId('teacher_id')->constrained('teachers')->onDelete('cascade');
             $table->timestamps();
         });
     }
