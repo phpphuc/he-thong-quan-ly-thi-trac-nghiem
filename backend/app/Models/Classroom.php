a7824745351024862b5030c1af7abb6ea984ed33
nnduy223 <20520471@gm.uit.edu.vn>
2024-12-30 18:12:43 +0700

    Update Classroom.php

--- backend/app/Models/Classroom.php
+++ backend/app/Models/Classroom.php
@@ -21,7 +21,7 @@ class Classroom extends Model
  
     public function subject()
     {
-        return $this->belongsTo(Subject::class, 'subject_id', 'subject_id');
+        return $this->belongsTo(Subject::class, 'subject_id', 'id');
     }
     public function students()
     {
