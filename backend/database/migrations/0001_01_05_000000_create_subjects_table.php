189c17188fef6ae22e5be6711820ac4425122cdb
nnduy223 <20520471@gm.uit.edu.vn>
2024-12-30 17:39:08 +0700

    Update 0001_01_05_000000_create_subjects_table.php

--- backend/database/migrations/0001_01_05_000000_create_subjects_table.php
+++ backend/database/migrations/0001_01_05_000000_create_subjects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('subject_name');
             $table->timestamps();
         });
     }
