bb420145533bedc53b8267bd55efabd9c11f1aaf
nnduy223 <20520471@gm.uit.edu.vn>
2025-01-03 17:24:36 +0700

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -46,18 +46,24 @@ class DatabaseSeeder extends Seeder
         }
 
         $subjects = [
-            [
-                "name" => "Anh văn 1",
-                "teacher_id" => "1",
-            ],
-            [
-                "name" => "Anh văn 2",
-                "teacher_id" => "1",
-            ]
-        ];
-        foreach ($subjects as $subject) {
-            Subject::create($subject);
-        }
+    [
+        "name" => "Anh văn",
+        "teacher_ids" => [1, 2, 3], // Mảng ID giáo viên
+    ]
+];
+
+$subjectController = new SubjectController();
+
+foreach ($subjects as $subject) {
+    // Tạo một request giả lập từ dữ liệu mảng
+    $request = new Request([
+        'name' => $subject['name'],
+        'teacher_ids' => $subject['teacher_ids'],
+    ]);
+
+    // Gọi phương thức store và truyền vào request
+    $subjectController->store($request);
+}
 
         $questions = file_get_contents(base_path('/database/fakedata/questions.json'));
         $questions = json_decode($questions, true);
@@ -66,32 +72,40 @@ class DatabaseSeeder extends Seeder
         }
 
 
-        $exams = [
+       $exams = [
+    [
+        "name" => "Kì thi cuối kì",
+        "school_board_id" => 1,
+        "examtype" => "NORMAL",
+        "subjects" => [
             [
-                "name" => "Final Exam",
-                "subject_id" => 1,
-                "teacher_id" => 1,
-                "time" => 60,
-                "examtype" => "NORMAL",
-                "Qtype1" => 1,
-                "Qtype2" => 1,
-                "Qtype3" => 1,
-                "Qnumber" => 3
-
-            ],
-            [
-                "name" => "Final Exam",
-                "subject_id" => 1,
-                "teacher_id" => 1,
-                "time" => 60,
-                "examtype" => "NORMAL",
-                "Qtype1" => 1,
-                "Qtype2" => 1,
-                "Qtype3" => 1,
-                "Qnumber" => 3
-
+                "id" => 1, // ID môn học
+                "time" => 60, // Thời gian làm bài
+                "Qtype1" => 1, // Số lượng câu hỏi loại 1
+                "Qtype2" => 1, // Số lượng câu hỏi loại 2
+                "Qtype3" => 1, // Số lượng câu hỏi loại 3
+                "Qnumber" => 3, // Tổng số câu hỏi
             ]
-        ];
+        ],
+        "teacher_ids" => [1, 2], // Danh sách ID giáo viên tham gia kỳ thi
+    ],
+    [
+        "name" => "Kì thi giữa kì",
+        "school_board_id" => 1,
+        "examtype" => "NORMAL",
+        "subjects" => [
+            [
+                "id" => 1, // ID môn học
+                "time" => 60, // Thời gian làm bài
+                "Qtype1" => 2, // Số lượng câu hỏi loại 1
+                "Qtype2" => 2, // Số lượng câu hỏi loại 2
+                "Qtype3" => 1, // Số lượng câu hỏi loại 3
+                "Qnumber" => 5, // Tổng số câu hỏi
+            ]
+        ],
+        "teacher_ids" => [2], // Danh sách ID giáo viên tham gia kỳ thi
+    ]
+];
 
         // foreach ($exams as $exam) {
         //     Exam::create($exam);
@@ -99,26 +113,35 @@ class DatabaseSeeder extends Seeder
 
         $examController = new ExamController();
 
-        foreach ($exams as $exam) {
-            $request = new Request($exam);
-            $examController->createExam($request);
-        }
+        foreach ($exams as $examData) {
+    // Tạo một request mới từ dữ liệu
+    $request = new Request($examData);
+    
+    // Gọi phương thức createExam để tạo kỳ thi
+    $examController->createExam($request);
+}
 
         $classes = [
             [
                 'name' => 'ENG_2024_1',
                 'subject_id' => 1,
-                'teacher_id' => 1,
+                'teacher_ids' => [1, 2]
             ],
             [
-                'name' => 'ENG_2024_2',
-                'subject_id' => 2,
-                'teacher_id' => 1
+                 'name' => 'ENG_2024_2',
+                 'subject_id' => 1,
+                 'teacher_ids' => [2]
             ]
         ];
 
         foreach ($classes as $class) {
-            Classroom::create($class);
+            $classData = Classroom::create([
+        'name' => $class['name'],
+        'subject_id' => $class['subject_id']
+    ]);
+
+    // Thêm giáo viên vào lớp học
+    $classData->teachers()->sync($class['teacher_ids']);
         }
         $classExams = [
             [
@@ -131,32 +154,42 @@ class DatabaseSeeder extends Seeder
             ],
         ];
 
-        foreach ($classExams as $classExam) {
-            ClassExam::create($classExam);
-        }
+        $examController = new ExamController();
+
+foreach ($classExams as $classExam) {
+    // Tạo một request giả lập từ dữ liệu mảng
+    $request = new Request([
+        'exam_id' => $classExam['exam_id']
+    ]);
+    
+    // Gọi phương thức addExamToClass và truyền vào classId
+    $examController->addExamToClass($request, $classExam['class_id']);
+}
 
 
         $classStudents = [
             [
                 'class_id' => 1,
-                'student_id' => 1,
-            ],
-            [
-                'class_id' => 1,
-                'student_id' => 2,
+                'student_ids' => [1,2,3],
             ],
             [
                 'class_id' => 2,
-                'student_id' => 3,
+                'student_ids' => [4,5,6],
             ],
-            [
-                'class_id' => 2,
-                'student_id' => 4,
-            ],
+           
         ];
 
-        foreach ($classStudents as $classStudent) {
-            ClassStudent::create($classStudent);
-        }
+        $classStudentController = new ClassStudentController();
+
+foreach ($classStudents as $classStudent) {
+    // Tạo một request giả lập từ dữ liệu mảng
+    $request = new Request([
+        'class_id' => $classStudent['class_id'],
+        'student_ids' => $classStudent['student_ids'],
+    ]);
+
+    // Gọi phương thức store và truyền vào request
+    $classStudentController->store($request);
+}
     }
 }
