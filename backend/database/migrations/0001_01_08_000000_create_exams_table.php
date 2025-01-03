0db97b4e41ce23688534b7a6ad57b9a9fd71f9dd
nnduy223 <20520471@gm.uit.edu.vn>
2025-01-03 17:32:28 +0700

    Update 0001_01_08_000000_create_exams_table.php

--- backend/database/migrations/0001_01_08_000000_create_exams_table.php
+++ backend/database/migrations/0001_01_08_000000_create_exams_table.php
@@ -14,16 +14,30 @@ return new class extends Migration
         Schema::create('exams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('school_board_id')->nullable();
             $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
-            // $table->string('subject_name');
-            // $table->foreignId('class_id')->constrained('classes')->onDelete('cascade');
-            $table->foreignId('teacher_id')->constrained('teachers')->onDelete('cascade');
-            $table->integer('time');
             $table->enum('examtype', ['NORMAL', 'GENERAL EXAM']);
-            $table->integer('Qtype1');
-            $table->integer('Qtype2');
-            $table->integer('Qtype3');
-            $table->integer('Qnumber');
+            $table->timestamps();
+            $table->foreign('school_board_id')->references('id')->on('school_boards');
+        });
+        // Tạo bảng trung gian liên kết kỳ thi với môn học
+        Schema::create('exam_subject', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exam_id')->constrained('exams')->onDelete('cascade');
+            $table->integer('time');
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
+            $table->integer('Qtype1')->default(0); // Số câu hỏi loại 1 (Nhận biết)
+            $table->integer('Qtype2')->default(0); // Số câu hỏi loại 2 (Thông hiểu)
+            $table->integer('Qtype3')->default(0); // Số câu hỏi loại 3 (Vận dụng)
+            $table->integer('Qnumber')->default(0); // Tổng số câu hỏi của môn học
+            $table->timestamps();
+        });
+
+        // Tạo bảng trung gian liên kết kỳ thi với giáo viên
+        Schema::create('exam_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exam_id')->constrained('exams')->onDelete('cascade');
+            $table->foreignId('teacher_id')->constrained('teachers')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -34,5 +48,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('exams');
+        Schema::dropIfExists('exam_teacher');
+        Schema::dropIfExists('exam_subject');
     }
 };
