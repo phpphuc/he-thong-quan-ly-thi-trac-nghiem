31b548e265978cb0033d1e18b591f1b3e69dc3e7
nnduy223 <20520471@gm.uit.edu.vn>
2025-01-06 07:10:39 +0700

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleMiddleware;
 use App\Http\Controllers\API\V1\QuestionController;
 use App\Http\Controllers\API\V1\SubjectController;
 use App\Http\Controllers\API\V1\ExamController;
@@ -20,7 +21,7 @@ Route::prefix('v1')->group(function () {
         return $request->user();
     })->middleware('auth:sanctum');
 
-    Route::get('/users', [App\Http\Controllers\API\V1\AuthController::class, 'index']);
+    //Route::get('/users', [App\Http\Controllers\API\V1\AuthController::class, 'index']);
     Route::post('/register', [App\Http\Controllers\API\V1\AuthController::class, 'register']);
     Route::post('/login', [App\Http\Controllers\API\V1\AuthController::class, 'login']);
     Route::post('/logout', [App\Http\Controllers\API\V1\AuthController::class, 'logout'])->middleware('auth:sanctum');
@@ -37,11 +38,13 @@ Route::prefix('v1')->group(function () {
     Route::delete('/questions/{id}', [QuestionController::class, 'destroy']);// Xóa câu hỏi
     
     Route::get('/exams', [ExamController::class, 'index']);// Lấy tất cả các kỳ thi
-    Route::post('/exams', [ExamController::class, 'createExam']);// Tạo một kỳ thi mới
+    Route::middleware(['auth:sanctum', 'role:school_board,teacher'])
+    ->post('/exams', [ExamController::class, 'createExam']);// Tạo một kỳ thi mới
     Route::get('/students/{id}/exams', [ExamController::class, 'getExamsForStudent']);// Lấy danh sách bài thi dành cho sinh viên
     Route::get('/exams/{id}', [ExamController::class, 'showExam']);// Lấy thông tin chi tiết kỳ thi
-    Route::post('/exams/{id}/submit', [ExamController::class, 'submitExam']);// Nộp bài thi và tính điểm
-    Route::get('/exams/{examId}/subjects-teachers', [ExamController::class, 'getExamSubjectsAndTeachers']);// Lấy các môn học và giáo viên dạy môn trong kỳ thi
+    Route::post('/exams/{examId}/submit', [ExamController::class, 'submitExam']);// Nộp bài thi và tính điểm
+    Route::get('/exams/{examId}/subjects-teachers', [ExamController::class, 'getExamSubjectsAndTeachers']);// Lấy các môn học và giáo viên dạy môn trong kỳ thi 
+    Route::get('/exams/{examId}/start/{studentId}', [ExamController::class, 'startExam']);// Route để lấy chi tiết bài thi cho sinh viên bắt đầu làm bài
 
     Route::get('/results', [ResultController::class, 'index']);// Hiển thị danh sách kết quả thi
     Route::get('/results/{id}', [ResultController::class, 'show']);// Hiển thị chi tiết kết quả của một bài thi
@@ -57,7 +60,8 @@ Route::prefix('v1')->group(function () {
     Route::get('/school-boards/{schoolBoardId}/exams', [SchoolBoardController::class, 'exams']);// Lấy danh sách các kỳ thi mà thành viên Ban giám hiệu giám sát
     Route::get('/school-boards/{schoolBoardId}/report', [SchoolBoardController::class, 'report']);// Tạo báo cáo kết quả kỳ thi của Ban giám hiệu
 
-    Route::post('/exams/{exam_id}/attach-questions', [ExamQuestionController::class, 'attachQuestions']);// Liên kết câu hỏi với kỳ thi
+    Route::middleware(['auth:sanctum', RoleMiddleware::class . ':teacher'])
+    ->post('/exams/{examId}/attach-questions', [ExamQuestionController::class, 'attachQuestions']);// Liên kết câu hỏi với kỳ thi
     Route::get('/exams/{exam_id}/show-questions', [ExamQuestionController::class, 'showQuestions']);// Xem các câu hỏi đã được liên kết với kỳ thi
     Route::post('/exams/{exam_id}/detach-questions', [ExamQuestionController::class, 'detachQuestions']);// Xóa liên kết câu hỏi khỏi kỳ thi
 
@@ -71,8 +75,7 @@ Route::prefix('v1')->group(function () {
     Route::put('/subjects/{id}', [SubjectController::class, 'update']);// Cập nhật thông tin môn học
     Route::delete('/subjects/{id}', [SubjectController::class, 'destroy']);// Xóa một môn học
     Route::get('/subjects/{id}/teachers', [SubjectController::class, 'getTeachers']);// Lấy danh sách các giáo viên giảng dạy một môn học
-    Route::post('/subjects/{id}/link-classes', [SubjectController::class, 'linkClass']);// Liên kết môn học với các lớp học
-    Route::post('/subjects/{id}/link-exams', [SubjectController::class, 'linkExam']);// Liên kết môn học với kỳ thi
+    Route::post('/subjects/{subjectId}/link-exams', [SubjectController::class, 'linkExam']);// Liên kết môn học với kỳ thi
 
     Route::get('/classes/{classId}/students', [ClassStudentController::class, 'index']);// Lấy danh sách sinh viên trong lớp học
     Route::post('/classes/students', [ClassStudentController::class, 'store']);// Thêm sinh viên vào lớp học
@@ -80,12 +83,10 @@ Route::prefix('v1')->group(function () {
     Route::get('/classes/{classId}/students/search', [ClassStudentController::class, 'search']);// Tìm kiếm sinh viên trong lớp học
     
     Route::get('/teachers', [TeacherController::class, 'index']);// Lấy danh sách giáo viên
-    Route::put('/teachers/{teacherId}', [TeacherController::class, 'update']);// Cập nhật thông tin giáo viên
+    Route::put('/teachers/{id}', [TeacherController::class, 'update']);// Cập nhật thông tin giáo viên
     Route::delete('/teachers/{teacherId}', [TeacherController::class, 'destroy']);// Xóa giáo viên
     Route::get('/teachers/{teacherId}/classes', [TeacherController::class, 'getClasses']);// Lấy danh sách lớp học của giáo viên
     Route::get('/teachers/{teacherId}/subjects', [TeacherController::class, 'getSubjects']);// Lấy danh sách môn học của giáo viên
-    Route::post('/teachers/organize-exam', [TeacherController::class, 'organizeExam']);// Tổ chức kỳ thi (chuyển tiếp request đến ExamController)
-    Route::post('/teachers/{teacherId}/add-classes', [TeacherController::class, 'addClassesToTeacher']);// Thêm lớp học vào giáo viên
 
     Route::get('/students', [StudentController::class, 'index']);// Hiển thị danh sách tất cả sinh viên
     Route::get('/students/{id}', [StudentController::class, 'show']);// Hiển thị thông tin sinh viên cụ thể
@@ -94,10 +95,9 @@ Route::prefix('v1')->group(function () {
     Route::get('/students/{studentId}/exam-history', [StudentController::class, 'examHistory']);// Xem lịch sử kết quả thi của sinh viên
     Route::get('/students/{studentId}/classes', [StudentController::class, 'getClasses']);// Lấy danh sách các lớp học mà sinh viên thuộc về
 
-    Route::get('/classes/{classId}/exams', [ClassExamController::class, 'getExamsForClass']);// Lấy danh sách tất cả các kỳ thi của lớp học
-    Route::post('/classes/{classId}/exams', [ClassExamController::class, 'addExamToClass']);// Thêm kỳ thi vào lớp học
-    Route::delete('/classes/{classId}/exams', [ClassExamController::class, 'removeExamFromClass']);// Xóa kỳ thi khỏi lớp học
-    Route::get('/classes/{classId}/exams/details', [ClassExamController::class, 'showExamsForClass']);// Hiển thị thông tin về các kỳ thi của lớp học
+    Route::post('/add-class-to-exam/{examId}', [ClassExamController::class, 'addClassToExam']);// Thêm lớp học vào kì thi
+    Route::post('/remove-class-from-exam/{examId}', [ClassExamController::class, 'removeClassFromExam']);// Xóa lớp học khỏi kì thi
+    Route::get('/show-exams-for-class/{classId}', [ClassExamController::class, 'showExamsForClass']);// Hiển thị thông tin về các kỳ thi của lớp học
 
 
 
